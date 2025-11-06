8310ff07caa0ad4bd678a06ce93748561a40e914
gemacjr <edmccormic@me.com>
2025-11-05 21:51:44 -0500

    initial commit

---
+++ src/main/java/com/swiftbeard/ai_resume_parser/AiResumeParserApplication.java
@@ -0,0 +1,13 @@
+package com.swiftbeard.ai_resume_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiResumeParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiResumeParserApplication.class, args);
+	}
+
+}
